976062d4c3fde1621f5e4e53793310a0f725a5a6
Klecianny Melo <kleciannymelo@gmail.com>
2023-10-17 23:37:14 -0300

    :sparkles: renderizando o primeiro JSON do desafio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [inputJSON, setInputJSON] = useState(null);
+  const [submitJSON, setSubimitJSON] = useState(false);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const jsonContent = event.target.result;
+        setInputJSON(jsonContent);
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  const handleJSONSubmit = () => {
+    setSubimitJSON(true);
+  };
+
   return (
     <div>
       <h1>Rinha de Frontend</h1>
+
+      <section>
+        <input
+          type="file"
+          onChange={handleFileChange}
+          placeholder="Insira o JSON aqui"
+        />
+        <button onClick={handleJSONSubmit}>Enviar</button>
+      </section>
+
+      {
+        submitJSON && inputJSON ? (
+          <section>
+            <pre>{JSON.stringify(JSON.parse(inputJSON), null, 2)}</pre>
+          </section>
+        ) : null
+      }
+
+      <footer>
+        <p>✨ Frontend is my passion ✨</p>
+      </footer>
     </div>
   );
 }
